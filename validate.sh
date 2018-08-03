175f4371e31078008bda454d4236bdadc46dddaa
jllopes <jlglopes96@gmail.com>
2018-08-03 11:53:40 +0100

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -3,7 +3,9 @@
 set -e
 
 echo "====> Checking if docker built successfully"
+echo "=====> Checking if neo4j-gremlin exists"
 test -d /tinkergraph/ext/neo4j-gremlin
+echo "=====> Checking if gremlin-groovy exists"
 test -d /tinkergraph/ext/gremlin-groovy
+echo "=====> Checking if tinkergraph-gremlin exists"
 test -d /tinkergraph/ext/tinkergraph-gremlin
-test -f /tinkergraph/conf/gsneo4j.3.3.yml
